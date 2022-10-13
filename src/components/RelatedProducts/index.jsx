cf69b0d41f808d421fc5d73d359fc60d421e7c15
Lawrence Good <lawrence.good55@gmail.com>
2022-10-13 14:38:39 -0700

    test

--- src/components/RelatedProducts/index.jsx
+++ src/components/RelatedProducts/index.jsx
@@ -5,17 +5,19 @@ export default function RelatedProducts() {
     <section>
       <div className="product-card">
         <div className="img"></div>
-        <div>
-          <h3>Category</h3>
-        </div>
-        <div>
-          <h2>Product name</h2>
-        </div>
-        <div>
-          <h3>Price</h3>
-        </div>
-        <div>
-          <h3>Ratings</h3>
+        <div className="info">
+          <div>
+            <h4>Category</h4>
+          </div>
+          <div>
+            <h2>Product name</h2>
+          </div>
+          <div>
+            <h4>Price</h4>
+          </div>
+          <div>
+            <h4>Ratings</h4>
+          </div>
         </div>
       </div>
     </section>
