44c9cf80def5badb94531e5d0a34ca0999b96dc8
Lawrence Good <lawrence.good55@gmail.com>
2022-10-15 10:45:41 -0700

    Fixed hot reload and added prod-card-skel

--- src/components/RelatedProducts/index.jsx
+++ src/components/RelatedProducts/index.jsx
@@ -1,16 +1,30 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import SkeletonCard from '../SkeletonCard'
 import '../../styles/relatedProducts.css'
+
+// type Product = {
+//   id: number
+//   name: string
+//   slogan: string
+//   description: string
+//   category: string
+//   default_price: string
+// }
+
+const FETCH_DELAY = 3000
 
 export default function RelatedProducts() {
   const [loading, setLoading] = useState(true)
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    axios.get('/products').then((response) => {
-      setLoading(false)
-      setProducts(response.data)
-    })
+    setTimeout(() => {
+      axios.get('/products').then((res) => {
+        setLoading(false)
+        setProducts(res.data)
+      })
+    }, FETCH_DELAY)
   }, [])
 
   console.log('products', products)
@@ -21,22 +35,37 @@ export default function RelatedProducts() {
         <h3>Related Products</h3>
       </div>
 
-      <div className="product-card">
-        <div className="img"></div>
-        <div className="info">
-          <div>
-            <h4>Category</h4>
+      <div className="related-products">
+        {!loading && !products.length && (
+          <div className="no-products">
+            <h3>No related products found</h3>
           </div>
-          <div>
-            <h2>Product name</h2>
-          </div>
-          <div>
-            <h4>Price</h4>
-          </div>
-          <div>
-            <h4>Ratings</h4>
-          </div>
-        </div>
+        )}
+
+        {loading &&
+          Array.from({ length: 5 }).map((_, i) => <SkeletonCard key={`skeleton-card-${i}`} />)}
+
+        {!loading &&
+          products.length &&
+          products.map((product) => (
+            <div className="product-card">
+              <div className="img"></div>
+              <div className="info">
+                <div>
+                  <h4>Category</h4>
+                </div>
+                <div>
+                  <h2>Product name</h2>
+                </div>
+                <div>
+                  <h4>Price</h4>
+                </div>
+                <div>
+                  <h4>Ratings</h4>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </section>
   )
---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,15 @@
+import '../styles/skeletonCard.css'
+
+export default function SkeletonCard() {
+  return (
+    <div className="product-card">
+      <div className="img skeleton"></div>
+      <div className="info">
+        <div className="skeleton skeleton-text" style={{ width: '50%' }}></div>
+        <div className="skeleton skeleton-text" style={{ width: '80%', height: '1.5rem' }}></div>
+        <div className="skeleton skeleton-text" style={{ width: '50%' }}></div>
+        <div className="skeleton skeleton-text" style={{ width: '30%' }}></div>
+      </div>
+    </div>
+  )
+}
