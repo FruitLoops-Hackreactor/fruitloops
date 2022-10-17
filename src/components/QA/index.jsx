4a4570408f4d9e9e90f49c01675b0d0d341fe611
jake-manning <jcmanning92@gmail.com>
2022-10-17 15:42:42 -0700

    Add button to load more questions if additional questions exist

--- src/components/QA/index.jsx
+++ src/components/QA/index.jsx
@@ -1,10 +1,13 @@
 import Question from './Question'
+import MoreQuestionsBtn from './MoreQuestionsBtn'
 import axios from 'axios'
 import { useState, useEffect, useContext } from 'react'
 import { AppContext } from '@/App'
 
 export default function QA() {
   let [questions, setQuestions] = useState([])
+  let [allQuestions, setAllQuestions] = useState([])
+  let [moreQuestions, setMoreQuestions] = useState(true)
   let { currentProduct } = useContext(AppContext)
   let [additionalQuestions, setAdditionalQuestions] = useState()
   console.log('currentProduct', currentProduct)
@@ -15,12 +18,20 @@ export default function QA() {
     axios
       .get('/qa/questions', {
         params: {
-          product_id: currentProduct.id,
+          product_id: 40343,
         },
       })
       .then((res) => {
-        setQuestions(res.data.results)
+        setAllQuestions(res.data.results)
         setAdditionalQuestions(0)
+        return res.data.results
+      })
+      .then((res) => {
+        if (res.length <= 4) {
+          setMoreQuestions(false)
+        } else {
+          setMoreQuestions(true)
+        }
       })
       .catch((err) => {
         console.error(err)
@@ -28,8 +39,15 @@ export default function QA() {
   }, [currentProduct])
 
   useEffect(() => {
-    setQuestions(questions.slice(0, 4 + additionalQuestions))
+    setQuestions(allQuestions.slice(0, 4 + additionalQuestions))
   }, [additionalQuestions])
+
+  const handleMoreQuestionsClick = () => {
+    if (allQuestions.length <= 4 + additionalQuestions + 2) {
+      setMoreQuestions(false)
+    }
+    setAdditionalQuestions(additionalQuestions + 2)
+  }
   // Added this so you can view the data results in the console
   console.log('questions', questions)
 
@@ -41,6 +59,9 @@ export default function QA() {
           return <Question question={question} key={index} />
         })}
       </div>
+      <span>
+        <MoreQuestionsBtn moreQuestions={moreQuestions} handleClick={handleMoreQuestionsClick} />
+      </span>
     </section>
   )
 }
