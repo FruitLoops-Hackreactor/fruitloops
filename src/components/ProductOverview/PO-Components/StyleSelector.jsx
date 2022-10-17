5e019eded7d5c46ce0ce6ad36f9b5ffaa593d0b6
Quyen Hoang <qduhoang@gmail.com>
2022-10-17 11:23:15 -0700

    Rough draft for product overview started - image gallery almost complete

---
+++ src/components/ProductOverview/PO-Components/StyleSelector.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from 'react'
+
+export default function StyleSelector({ currentProduct }) {
+  return (
+    <div className="style-selector">
+      <div>STYLE &gt; SELECTED STYLE</div>
+    </div>
+  )
+}
--- src/components/ProductOverview/index.jsx
+++ src/components/ProductOverview/index.jsx
@@ -1,27 +1,33 @@
-import SearchBar from './SearchBar'
-import '../../styles/productOverview.css'
+import { AppContext } from '@/App'
+import { useContext } from 'react'
+import SearchBar from './PO-Components/SearchBar'
+import ImageGallery from './PO-Components/ImageGallery'
+import Description from './PO-Components/Description'
+import ProductInformation from './PO-Components/ProductInformation'
+import '@/styles/productOverview.css'
 
 export default function ProductOverview() {
+  const { products, currentProduct } = useContext(AppContext)
+
   return (
-    <section className="product-overview">
+    <div className="product-overview">
       <div className="navbar">
         <SearchBar />
       </div>
-      <div className="image-gallery">
-        <h4>Image Gallery</h4>
-      </div>
-      <div className="product-info">
-        <h4>Product Information</h4>
-      </div>
-      <div className="style-selector">
-        <h4>Style Selector</h4>
-      </div>
-      <div className="add-to-cart">
-        <h4>Add to Cart</h4>
+      <div className="main-container">
+        <div className="image-gallery">
+          <br></br>
+          <ImageGallery products={products} currentProduct={currentProduct} />
+        </div>
+        <div className="product-info">
+          <h4>Product Information</h4>
+          <ProductInformation currentProduct={currentProduct} />
+        </div>
       </div>
       <div className="description">
         <h4>Description</h4>
+        <Description />
       </div>
-    </section>
+    </div>
   )
 }
