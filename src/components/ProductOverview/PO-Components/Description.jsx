e92a14b718b0d66d2ca3114b5c3b219f050dc6d4
Quyen Hoang <qduhoang@gmail.com>
2022-10-18 02:15:40 -0700

    Refactored css grid for description and code in description.jsx

--- src/components/ProductOverview/PO-Components/Description.jsx
+++ src/components/ProductOverview/PO-Components/Description.jsx
@@ -9,9 +9,7 @@ export default function Description({ product, id }) {
   return (
     <div className="description-container">
       <div className="slogan">{product.slogan}</div>
-      <br></br>
       <div className="desc-summary">{product.description}</div>
-      <br></br>
       <ul className="features">{listItems}</ul>
     </div>
   )
