d9890ac45240d15e5a36c6fbce804556e9dbbf3e
Valphalia Hoopes <valphaliahoopes@gmail.com>
2022-10-18 19:02:17 -0600

    Get some reviews on the screen

--- src/components/RatingsReviews/index.jsx
+++ src/components/RatingsReviews/index.jsx
@@ -1,5 +1,32 @@
+import { useContext, useEffect, useState} from 'react'
+import { AppContext } from '@/App'
+import ReviewsList from './ReviewsList'
+import axios from 'axios'
+
 export default function RatingsReviews() {
-  return (<section>
-    <h3>Ratings and Reviews</h3>
-  </section>)
+  const { currentProduct } = useContext(AppContext)
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    if (!currentProduct) return
+    axios
+      .get('/reviews', {
+        params: {
+          product_id: currentProduct.id,
+        },
+      })
+      .then((res) => {
+        setReviews(res.data.results)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [currentProduct])
+
+  return (
+    <section>
+      <h3 className='section-title'>RATINGS AND REVIEWS</h3>
+      <ReviewsList reviews={reviews} />
+    </section>
+  )
 }
