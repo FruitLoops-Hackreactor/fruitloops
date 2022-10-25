d7688fb4a242f96a044e5f8907061d6af1de183b
Lawrence Good <lawrence.good55@gmail.com>
2022-10-25 13:57:18 -0700

    Fixed a test

--- src/__tests__/relatedProducts/index.test.jsx
+++ src/__tests__/relatedProducts/index.test.jsx
@@ -1,7 +1,6 @@
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import RelatedProducts from '@/components/RelatedProducts'
 import productMock from '../mocks/product'
-import { act } from 'react-dom/test-utils'
 
 jest.mock('axios', () => ({
   get: jest.fn(() => Promise.resolve({ data: [] })),
@@ -17,12 +16,11 @@ jest.mock('@/utils/products', () => ({
 
 describe('Related Products - index', () => {
   it('should render with a current product', async () => {
+    // Need to wrap in act to avoid a warning about state updates
     await act(async () => {
       render(<RelatedProducts />)
     })
 
-    await waitFor(() => {
-      expect(screen.queryByTestId('related-products')).toBeTruthy()
-    })
+    expect(screen.queryByTestId('related-products')).toBeTruthy()
   })
 })
