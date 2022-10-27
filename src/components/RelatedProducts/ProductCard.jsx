2ac0ca7fa3b071b0f5942440d5bb67d39f098039
Lawrence Good <lawrence.good55@gmail.com>
2022-10-27 15:48:00 -0700

    Update eslint, using SVGs instead of library

--- src/components/RelatedProducts/ProductCard.jsx
+++ src/components/RelatedProducts/ProductCard.jsx
@@ -1,4 +1,3 @@
-import { IconStar, IconX } from '@tabler/icons'
 import { useRef } from 'react'
 
 export default function ProductCard({ product, onProductClick, action, actionHandler }) {
@@ -15,15 +14,50 @@ export default function ProductCard({ product, onProductClick, action, actionHan
   }
 
   return (
-    <div ref={cardRef} key={product.id} className="product-card" onClick={handleCardClick}>
+    <button
+      ref={cardRef}
+      key={product.id}
+      className="product-card"
+      onClick={handleCardClick}
+      onKeyDown={handleCardClick}
+    >
       <div className="img">
         <img src={product.styles[0].photos[0].thumbnail_url} alt={product.name} />
       </div>
       <div ref={actionRef} className="action">
         {action === 'compare' ? (
-          <IconStar size={24} color="gold" fill="gold" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-star"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="gold"
+            fill="gold"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+          </svg>
         ) : action === 'remove' ? (
-          <IconX size={28} color="black" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-x"
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            strokeWidth="2"
+            stroke="black"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
         ) : null}
       </div>
       <div className="info">
@@ -44,6 +78,6 @@ export default function ProductCard({ product, onProductClick, action, actionHan
           )}
         </div>
       </div>
-    </div>
+    </button>
   )
 }
--- src/components/RelatedProducts/Products.jsx
+++ src/components/RelatedProducts/Products.jsx
@@ -3,6 +3,24 @@ import Carousel from './Carousel'
 import ProductCard from './ProductCard'
 import SkeletonCard from '../SkeletonCard'
 
+const Check = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-check"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M5 12l5 5l10 -10" />
+  </svg>
+)
+
 export default function Products({ max, loading, currentProduct, relatedProducts }) {
   const setModalContent = useStore('modalContent')[1]
   const currFeatures = !currentProduct
@@ -55,9 +73,9 @@ export default function Products({ max, loading, currentProduct, relatedProducts
           ) : (
             features.map(({ feature, currentProduct, relatedProduct }) => (
               <div className="row" key={feature}>
-                <span>{currentProduct === 'true' ? <IconCheck size={22} /> : currentProduct}</span>
+                <span>{currentProduct === 'true' ? <Check /> : currentProduct}</span>
                 <span className="feature">{feature}</span>
-                <span>{relatedProduct === 'true' ? <IconCheck size={22} /> : relatedProduct}</span>
+                <span>{relatedProduct === 'true' ? <Check /> : relatedProduct}</span>
               </div>
             ))
           )}
