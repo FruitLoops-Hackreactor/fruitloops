d18e83b8bc17a97c394234266ce54cefdfc31838
Quyen Hoang <qduhoang@gmail.com>
2022-10-27 17:59:46 -0700

    Fixed edge case in description where the li element value was null

--- src/components/ProductOverview/Description.jsx
+++ src/components/ProductOverview/Description.jsx
@@ -24,7 +24,7 @@ export default function Description({ product }) {
         <li>{`${feature.feature}: ${feature.value}`}</li>
       </div>
     ) : (
-      <div key={index}>
+      <div className="li-container" key={index}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="icon icon-tabler icon-tabler-check"
